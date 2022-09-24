6912be4bbef8d1117251a8d9b50f6d356fd3a72e
John Calderon <johncalesp@gmail.com>
2022-09-23 21:42:24 -0400

    finished navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
-import Dashboard from './Dashboard';
 import Error from './Error';
 import Landing from './Landing';
 import Register from './Register';
-
-export { Dashboard, Error, Landing, Register };
+import ProtectedRoute from './ProtectedRoute';
+export { Error, Landing, Register, ProtectedRoute };
